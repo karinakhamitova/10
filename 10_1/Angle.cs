41a43064c71e8c49fd825500fd9a9e50f2338ec1
TopFps <TopFps@DESKTOP-UUEU6H0>
2022-11-06 20:22:17 +0300

    Добавьте файлы проекта.

---
+++ 10_1/Angle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_1
+{
+    internal class Angle
+    {
+        int gradus;
+        int minute;
+        int sec;
+        int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+
+                gradus = value;
+            }
+        }
+        int Minute
+        {
+            get
+            {
+                return minute;
+            }
+            set
+            {
+                Gradus += value / 60;
+                minute = value % 60;
+            }
+        }
+        int Sec
+        {
+            get
+            {
+                return minute;
+            }
+            set
+            {
+                Gradus += value / 60;
+                sec = value % 60;
+            }
+        }
+        public Angle(int gradus, int minute, int sec)
+        {
+            this.Gradus = gradus;
+            this.Minute = minute;
+            this.Sec = sec;
+        }
+        public double ToRadian()
+        {
+            return (gradus * 60 * 60 + minute * 60.0 + sec)*Math.PI/(3600*180);
+        }
+    }
+}
---
+++ 10_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение угла:");
+            int gradus_ = Convert.ToInt32(Console.ReadLine());
+            int minute_ = Convert.ToInt32(Console.ReadLine());
+            int sec_ = Convert.ToInt32(Console.ReadLine());
+            Angle angle = new Angle(gradus_, minute_, sec_);
+            double total = angle.ToRadian();
+            Console.WriteLine("{0}° {1}' {2}'' = {3:f3} радиан", gradus_, minute_, sec_, total);
+            Console.ReadKey();
+        }
+
+    }
+
+
+
+
+}
